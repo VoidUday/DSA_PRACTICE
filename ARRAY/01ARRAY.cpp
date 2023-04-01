bba96d61a00ac70b2f0a3ac12b523a08306bf697
voiduday <udaypratapyadaw03@gmail.com>
2023-04-01 14:45:01 +0530

    move the changes

---
+++ ARRAY/01ARRAY.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+using namespace std;
+int main(){
+  int n;
+   cin >> n;
+   //Initialization
+   int arr[n];
+   //Taking input
+   for(int i=0; i<n; i++){
+   cin >> arr[i];
+}
+}
