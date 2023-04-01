16f4616f3f4d1daf40169562a5181604f01580cc
voiduday <udaypratapyadaw03@gmail.com>
2023-04-01 14:55:29 +0530

    done

--- ARRAY/01ARRAY.cpp
+++ ARRAY/01ARRAY.cpp
@@ -3,10 +3,20 @@ using namespace std;
 int main(){
   int n;
    cin >> n;
-   //Initialization
+   //Declare
+
    int arr[n];
    //Taking input
+
    for(int i=0; i<n; i++){
    cin >> arr[i];
-}
+   }
+   //Accessing of array
+   
+   for (int i = 0; i < n; i++)
+   {
+      cout << arr[i] << " ";
+   }
+   
+
 }
