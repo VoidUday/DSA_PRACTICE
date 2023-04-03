2fb028688b3a92a4296a521be7ea2c44d2d04fe7
voiduday <udaypratapyadaw03@gmail.com>
2023-04-03 17:13:08 +0530

    swap completed

--- ARRAY/02_Array.cpp
+++ ARRAY/02_Array.cpp
@@ -10,7 +10,14 @@ void printArray(int arr[], int n){
 void swapArray(int arr[], int n){
     for(int i=0; i<n; i+=2){
         if(i+1 < n){
-            swap(arr[i] , arr[i+1]);
+            //Inbuilt Swap function 
+
+            // swap(arr[i] , arr[i+1]); 
+
+            //Swapping code
+            int temp = arr[i];
+            arr[i] = arr[i+1];
+            arr[i+1] = temp;
         }
     }
 }
